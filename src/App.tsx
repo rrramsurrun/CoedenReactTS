4d77acac0eaeeca0d980bd47e3875dfb0bb96207
Robin Ramsurrun <rrramsurrun@gmail.com>
2024-05-20 17:42:21 +0100

    Added Redux functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
 import { useEffect, useState } from "react";
 import treeLogo from "./assets/treeImage.png";
 import "./App.css";
-import { Tree } from "./features/trees/treeSlice";
-import { getSampleTrees } from "./features/trees/treesAPI";
+import {
+  Tree,
+  getTrees,
+  selectStatus,
+  selectTrees,
+} from "./features/trees/treeSlice";
 import { ClipLoader } from "react-spinners";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 function App() {
-  const [findingTree, setFindingTree] = useState(false);
-  const [singleTree, setSingleTree] = useState<Tree | null>(null);
-  useEffect(() => {
-    if (findingTree) {
-      getSampleTrees().then((data) => {
-        setSingleTree(data[Math.floor(Math.random() * data.length)]);
-        setFindingTree(false);
-      });
-    }
-  }, [findingTree]);
+  const dispatch = useAppDispatch();
+  const singleTree = useAppSelector(selectTrees);
+  const findingTree = useAppSelector(selectStatus);
 
   function TreeSection() {
-    if (findingTree) {
+    if (findingTree === "loading") {
       return (
         <div>
           <ClipLoader />
         </div>
       );
     }
-    if (singleTree) {
+    if (singleTree.length > 0) {
       return (
         <div>
-          <p>{singleTree.common_name}</p>
-          <p>{singleTree.latitude}</p>
-          <p>{singleTree.longitude}</p>
+          <p>{singleTree[0].common_name}</p>
+          <p>{singleTree[0].latitude}</p>
+          <p>{singleTree[0].longitude}</p>
         </div>
       );
+    }
+  }
+  function buttonText() {
+    switch (findingTree) {
+      case "successful":
+        return "Click to find another tree";
+      default:
+        return "Click to retrieve a tree";
     }
   }
 
@@ -43,10 +49,11 @@ function App() {
       </div>
       <h1>Coeden</h1>
       <div className="card">
-        <button disabled={findingTree} onClick={() => setFindingTree(true)}>
-          {singleTree
-            ? "Click to find another tree"
-            : "Click to retrieve a tree"}
+        <button
+          disabled={findingTree === "loading"}
+          onClick={() => dispatch(getTrees())}
+        >
+          {buttonText()}
         </button>
         {TreeSection()}
       </div>
--- src/features/trees/treeSlice.ts
+++ src/features/trees/treeSlice.ts
@@ -1,4 +1,6 @@
-// import { createAppSlice } from "../../redux/createAppSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAppSlice } from "../../redux/createAppSlice";
+import { getSampleTrees } from "./treesAPI";
 
 export type Tree = {
   id: number;
@@ -10,19 +12,49 @@ export type Tree = {
   latitude: number;
 };
 
-// export type TreeSliceState = {
-//   trees: Tree[];
-//   status: "idle" | "loading" | "failed"
-// }
+export type TreeSliceState = {
+  trees: Tree[];
+  status: "idle" | "loading" | "failed" | "successful";
+  error: string | null;
+};
 
-// const initialState: TreeSliceState = {
-//   trees: [],
-//   status: "idle"
-// }
+const initialState: TreeSliceState = {
+  trees: [],
+  status: "idle",
+  error: null,
+};
 
-// export const treeSlice = createAppSlice({
-//   name:'tree',
-//   initialState,
-//   reducers: null,
+export const treeSlice = createAppSlice({
+  name: "tree",
+  initialState,
+  reducers: (create) => ({
+    getTrees: create.asyncThunk(
+      async () => {
+        const trees = await getSampleTrees();
+        return trees;
+      },
+      {
+        pending: (state) => {
+          state.status = "loading";
+        },
+        fulfilled: (state, action) => {
+          state.status = "successful";
+          state.trees = [
+            action.payload[Math.floor(Math.random() * action.payload.length)],
+          ];
+        },
+        rejected: (state) => {
+          state.status = "failed";
+        },
+      }
+    ),
+  }),
+  selectors: {
+    selectTrees: (tree) => tree.trees,
+    selectStatus: (tree) => tree.status,
+  },
+});
 
-// })
+export const { getTrees } = treeSlice.actions;
+
+export const { selectTrees, selectStatus } = treeSlice.selectors;
